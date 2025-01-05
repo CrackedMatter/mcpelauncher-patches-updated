e509c3ce39da36381fc23e34e37e163ca6d7cd31
CrackedMatter <81803926+CrackedMatter@users.noreply.github.com>
2025-01-05 14:17:27 +0100

    Add .clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,9 @@ struct AppPlatform_vtable {
 #else
     void* pad0[39];
 #endif
-    bool (* blankLineDismissesChat)(void*);
+    bool (*blankLineDismissesChat)(void*);
     void* pad1[42];
-    bool (* supportsFilePicking)(void*);
+    bool (*supportsFilePicking)(void*);
 #if MC_VERSION >= 1021020
     void* pad2[115];
 #elif MC_VERSION >= 1021000
@@ -22,7 +22,7 @@ struct AppPlatform_vtable {
 #else
     void* pad2[112];
 #endif
-    int (* getDefaultNetworkMaxPlayers)(void*);
+    int (*getDefaultNetworkMaxPlayers)(void*);
 #if MC_VERSION >= 1021050
     void* pad3[34];
 #elif MC_VERSION >= 1021040
@@ -30,7 +30,7 @@ struct AppPlatform_vtable {
 #else
     void* pad3[32];
 #endif
-    int (* getMaxSimRadiusInChunks)(void*);
+    int (*getMaxSimRadiusInChunks)(void*);
 #if MC_VERSION >= 1021030
     void* pad4[18];
 #elif MC_VERSION >= 1020060
@@ -38,47 +38,50 @@ struct AppPlatform_vtable {
 #else
     void* pad4[18];
 #endif
-    std::string (* getEdition)(void*);
+    std::string (*getEdition)(void*);
 };
 
 extern "C" [[gnu::visibility("default")]] void mod_preinit() {
     auto h = dlopen("libmcpelauncher_mod.so", 0);
 
-    auto mcpelauncher_preinithook = (void (*)(const char*, void*, void**)) dlsym(h, "mcpelauncher_preinithook");
+    auto mcpelauncher_preinithook = (void (*)(const char*, void*, void**))dlsym(h, "mcpelauncher_preinithook");
 
     static ANativeActivity_createFunc* onCreate_orig;
 
-    mcpelauncher_preinithook("ANativeActivity_onCreate", (void*) +[](ANativeActivity* activity, void* savedState, size_t savedStateSize) {
-        onCreate_orig(activity, savedState, savedStateSize);
+    mcpelauncher_preinithook(
+        "ANativeActivity_onCreate",
+        (void*)+[](ANativeActivity* activity, void* savedState, size_t savedStateSize) {
+            onCreate_orig(activity, savedState, savedStateSize);
 
-        static auto onStart_orig = activity->callbacks->onStart;
+            static auto onStart_orig = activity->callbacks->onStart;
 
-        activity->callbacks->onStart = +[](ANativeActivity* activity) {
-            onStart_orig(activity);
+            activity->callbacks->onStart = +[](ANativeActivity* activity) {
+                onStart_orig(activity);
 
-            auto vt = *((AppPlatform_vtable****) activity->instance)[0][1];
+                auto vt = *((AppPlatform_vtable****)activity->instance)[0][1];
 
-            vt->blankLineDismissesChat = +[](void*) -> bool {
-                return true;
+                vt->blankLineDismissesChat = +[](void*) -> bool {
+                    return true;
+                };
+
+                vt->supportsFilePicking = +[](void*) -> bool {
+                    return true;
+                };
+
+                vt->getDefaultNetworkMaxPlayers = +[](void*) -> int {
+                    return 8;
+                };
+
+                vt->getMaxSimRadiusInChunks = +[](void*) -> int {
+                    return 12;
+                };
+
+                vt->getEdition = +[](void*) -> std::string {
+                    return "win10";
+                };
             };
-
-            vt->supportsFilePicking = +[](void*) -> bool {
-                return true;
-            };
-
-            vt->getDefaultNetworkMaxPlayers = +[](void*) -> int {
-                return 8;
-            };
-
-            vt->getMaxSimRadiusInChunks = +[](void*) -> int {
-                return 12;
-            };
-
-            vt->getEdition = +[](void*) -> std::string {
-                return "win10";
-            };
-        };
-    }, (void**) &onCreate_orig);
+        },
+        (void**)&onCreate_orig);
 }
 
 extern "C" [[gnu::visibility("default")]] void mod_init() {}
