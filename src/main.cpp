983539227e147f26ed336e39c9dd148153727f50
CrackedMatter <81803926+CrackedMatter@users.noreply.github.com>
2025-09-10 15:18:32 +0200

    Refactor

--- src/main.cpp
+++ src/main.cpp
@@ -10,74 +10,6 @@
 #include <string>
 #include <tuple>
 
-template<int32_t>
-struct AppPlatform_vtable;
-
-#define MC_VERSION 1021090
-#include "vtable.inl"
-#define MC_VERSION 1021080
-#include "vtable.inl"
-#define MC_VERSION 1021070
-#include "vtable.inl"
-#define MC_VERSION 1021060
-#include "vtable.inl"
-#define MC_VERSION 1021050
-#include "vtable.inl"
-#define MC_VERSION 1021040
-#include "vtable.inl"
-#define MC_VERSION 1021030
-#include "vtable.inl"
-#define MC_VERSION 1021020
-#include "vtable.inl"
-#define MC_VERSION 1021000
-#include "vtable.inl"
-#define MC_VERSION 1020080
-#include "vtable.inl"
-#define MC_VERSION 1020070
-#include "vtable.inl"
-#define MC_VERSION 1020060
-#include "vtable.inl"
-#define MC_VERSION 1020050
-#include "vtable.inl"
-
-struct AppPlatform_offsets {
-    uint16_t blankLineDismissesChat;
-    uint16_t supportsFilePicking;
-    uint16_t getDefaultNetworkMaxPlayers;
-    uint16_t getMaxSimRadiusInChunks;
-    uint16_t getEdition;
-
-    template<int32_t Version>
-    static consteval AppPlatform_offsets get() {
-        using T = AppPlatform_vtable<Version>;
-        return {
-            .blankLineDismissesChat      = offsetof(T, blankLineDismissesChat),
-            .supportsFilePicking         = offsetof(T, supportsFilePicking),
-            .getDefaultNetworkMaxPlayers = offsetof(T, getDefaultNetworkMaxPlayers),
-            .getMaxSimRadiusInChunks     = offsetof(T, getMaxSimRadiusInChunks),
-            .getEdition                  = offsetof(T, getEdition),
-        };
-    }
-
-    static AppPlatform_offsets get(int minor, int patch) {
-        if (minor >= 21) {
-            if (patch >= 90) return get<1021090>();
-            if (patch >= 80) return get<1021080>();
-            if (patch >= 70) return get<1021070>();
-            if (patch >= 60) return get<1021060>();
-            if (patch >= 50) return get<1021050>();
-            if (patch >= 40) return get<1021040>();
-            if (patch >= 30) return get<1021030>();
-            if (patch >= 20) return get<1021020>();
-            return get<1021000>();
-        }
-        if (patch >= 80) return get<1020080>();
-        if (patch >= 70) return get<1020070>();
-        if (patch >= 60) return get<1020060>();
-        return get<1020050>();
-    }
-};
-
 extern "C" [[gnu::visibility("default")]] void mod_preinit() {}
 
 extern "C" [[gnu::visibility("default")]] void mod_init() {
@@ -128,26 +60,52 @@ extern "C" [[gnu::visibility("default")]] void mod_init() {
 
     auto typeinfo_name = hat::find_pattern(range1, hat::object_to_signature("19AppPlatform_android")).get();
     auto typeinfo      = hat::find_pattern(range2, hat::object_to_signature(typeinfo_name)).get() - sizeof(void*);
-    auto vtable        = hat::find_pattern(range2, hat::object_to_signature(typeinfo)).get() + sizeof(void*);
-    auto offsets       = AppPlatform_offsets::get(minor, patch);
+    auto vtable        = reinterpret_cast<void**>(hat::find_pattern(range2, hat::object_to_signature(typeinfo)).get() + sizeof(void*));
 
-    *(void**)(vtable + offsets.blankLineDismissesChat) = (void*)+[](void*) -> bool {
+    int32_t version = major * 1000000 + minor * 1000 + patch;
+    size_t index    = 0;
+
+    size_t blankLineDismissesChat      = index += version >= 1021070   ? 41
+                                                  : version >= 1021060 ? 42
+                                                  : version >= 1021040 ? 40
+                                                  : version >= 1021020 ? 38
+                                                                       : 39;
+    size_t supportsFilePicking         = index += version >= 1021090   ? 43
+                                                  : version >= 1021080 ? 44
+                                                                       : 43;
+    size_t getDefaultNetworkMaxPlayers = index += version >= 1021080   ? 115
+                                                  : version >= 1021020 ? 116
+                                                  : version >= 1021000 ? 115
+                                                  : version >= 1020080 ? 114
+                                                                       : 113;
+    size_t getMaxSimRadiusInChunks     = index += version >= 1021070   ? 38
+                                                  : version >= 1021060 ? 37
+                                                  : version >= 1021050 ? 35
+                                                  : version >= 1021040 ? 34
+                                                                       : 33;
+    size_t getEdition                  = index += version >= 1021030   ? 19
+                                                  : version >= 1020060 ? 18
+                                                                       : 19;
+
+    (void)index;
+
+    vtable[blankLineDismissesChat] = (void*)+[](void*) -> bool {
         return true;
     };
 
-    *(void**)(vtable + offsets.supportsFilePicking) = (void*)+[](void*) -> bool {
+    vtable[supportsFilePicking] = (void*)+[](void*) -> bool {
         return true;
     };
 
-    *(void**)(vtable + offsets.getDefaultNetworkMaxPlayers) = (void*)+[](void*) -> int {
+    vtable[getDefaultNetworkMaxPlayers] = (void*)+[](void*) -> int {
         return 8;
     };
 
-    *(void**)(vtable + offsets.getMaxSimRadiusInChunks) = (void*)+[](void*) -> int {
+    vtable[getMaxSimRadiusInChunks] = (void*)+[](void*) -> int {
         return 12;
     };
 
-    *(void**)(vtable + offsets.getEdition) = (void*)+[](void*) -> std::string {
+    vtable[getEdition] = (void*)+[](void*) -> std::string {
         return "win10";
     };
 }
